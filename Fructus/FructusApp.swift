9cd97fce0856aa56c4fc1af7898cfec94e18c0ed
Mobin Ahmed <mobinahmed4321@gmail.com>
2024-04-02 00:04:18 -0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Apptycoons on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
