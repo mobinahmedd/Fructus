264928432634851ccf9abc94b2f603059b4e685b
Mobin Ahmed <mobinahmed4321@gmail.com>
2024-04-04 11:09:33 -0700

    "Added settings sheet"

---
+++ Fructus/App/SettingsRowView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsRowView.swift
+//  Fructus
+//
+//  Created by Apptycoons on 04/04/2024.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsRowView()
+}
---
+++ Fructus/View/SettingsLabelView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsLabelView.swift
+//  Fructus
+//
+//  Created by Apptycoons on 04/04/2024.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsLabelView()
+}
